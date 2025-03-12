3d8e3ed3d5a2d4f97ae18b2e92cc4dd6257fce2f
Davi Jose <85004283+Davi-pontes@users.noreply.github.com>
2025-03-11 23:33:12 -0300

    feat: create user entity

---
+++ src/main/java/com/davijose/challenge_foursales/domain/user/RoleUser.java
@@ -0,0 +1,6 @@
+package com.davijose.challenge_foursales.domain.user;
+
+public enum RoleUser {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/davijose/challenge_foursales/domain/user/User.java
@@ -0,0 +1,30 @@
+package com.davijose.challenge_foursales.domain.user;
+
+import com.davijose.challenge_foursales.domain.category.Category;
+import com.davijose.challenge_foursales.domain.order.Order;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+@Table(name = "user")
+@Entity(name = "User")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private String Password;
+    @Enumerated(EnumType.STRING)
+    private RoleUser roleUser;
+}
