b7ae994c34c04a195ec13709b1fb28da21d4e3da
Davi Jose <85004283+Davi-pontes@users.noreply.github.com>
2025-03-12 00:01:39 -0300

    fix: bug fix incorret import in CategoryRepository

--- src/main/java/com/davijose/challenge_foursales/repositories/CategoryRepository.java
+++ src/main/java/com/davijose/challenge_foursales/repositories/CategoryRepository.java
@@ -1,8 +1,9 @@
 package com.davijose.challenge_foursales.repositories;
 
+import com.davijose.challenge_foursales.domain.category.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CategoryRepository extends JpaRepository<CategoryRepository, UUID> {
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
 }
