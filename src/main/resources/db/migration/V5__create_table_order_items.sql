1a7effbe72812fee5753cea971862ca88fccfbd8
Davi Jose <85004283+Davi-pontes@users.noreply.github.com>
2025-03-12 11:58:24 -0300

    feat: create migration the table order items

---
+++ src/main/resources/db/migration/V5__create_table_order_items.sql
@@ -0,0 +1,9 @@
+CREATE TABLE order_items (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    order_id BIGINT NOT NULL,
+    product_id CHAR(36) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_order_items_order FOREIGN KEY (order_id) REFERENCES orders(id),
+    CONSTRAINT fk_order_items_product FOREIGN KEY (product_id) REFERENCES products(id)
+);
