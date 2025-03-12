34ee249b34add8225492a10086e55344cd23e76f
Davi Jose <85004283+Davi-pontes@users.noreply.github.com>
2025-03-11 23:32:21 -0300

    feat: initial config

---
+++ src/main/java/com/davijose/challenge_foursales/ChallengeFoursalesApplication.java
@@ -0,0 +1,13 @@
+package com.davijose.challenge_foursales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeFoursalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeFoursalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davijose/challenge_foursales/ChallengeFoursalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davijose.challenge_foursales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeFoursalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
