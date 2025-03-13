300d69a9956e08f9499e756328c753e1b3cf4bc2
Davi Jose <85004283+Davi-pontes@users.noreply.github.com>
2025-03-13 00:31:15 -0300

    feat: Delete entity category

--- src/main/java/com/davijose/challenge_foursales/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.davijose.challenge_foursales.repositories;
-
-import com.davijose.challenge_foursales.domain.category.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CategoryRepository extends JpaRepository<Category, UUID> {
-}
